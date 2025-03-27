ec33239802be88eda475376f6f705a0fbeab80c0
Your Name <you@example.com>
2025-03-27 06:45:03 +0300

    added changes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //find the last elements of the following arrays
 let arr1 = [3,7,34,90,12];
 let arr2 =  [true, "green", "where",12,56];
-console.log(arr1.slice(4));
-console.log(arr2.slice(4));
+console.log(arr1[arr1.length - 1]); 
+console.log(arr2[arr2.length - 1]);
 
 //Write a JS program that will join the following array elements into a string
 let myPets = ["Cow", "Bird", "Snake", "Dog"];
